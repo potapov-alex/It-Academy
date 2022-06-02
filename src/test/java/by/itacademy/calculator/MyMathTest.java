8dd3ade1c866cdec3b04e1bb9d3975f5eadadb77
Alexey Potapov <potapov.alex@hotmail.com>
2022-06-02 13:38:09 +0300

    add MyMath tests

--- src/test/java/by/itacademy/calculator/MyMathTest.java
+++ src/test/java/by/itacademy/calculator/MyMathTest.java
@@ -1,20 +1,43 @@
 package by.itacademy.calculator;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.assertTrue;
+public class MyMathTest {
 
-/**
- * Unit test for simple App.
- */
-public class MyMathTest
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAddPosNumbers() {
+        double a = 10.3;
+        double b = 6.1;
+        double result = MyMath.add(a, b);
+        double expectedResult = 16.4;
+        Assertions.assertEquals(expectedResult,result, "Wrong result of method 'add' ");
+    }
+
+    @Test
+    public void shouldSubtractPosNumbers() {
+        double a = 10.3;
+        double b = 6.1;
+        double result = MyMath.subtract(a, b);
+        double expectedResult = 4.2;
+        Assertions.assertEquals(expectedResult,result, "Wrong result of method 'subtract' ");
+    }
+
+    @Test
+    public void shouldMultiplyPosNumbers() {
+        double a = 10.3;
+        double b = 6.1;
+        double result = MyMath.multiply(a, b);
+        double expectedResult = 62.83;
+        Assertions.assertEquals(expectedResult,result, "Wrong result of method 'multiply' ");
+    }
+
+    @Test
+    public void shouldDivPosNumbers() {
+        double a = 10.3;
+        double b = 6.1;
+        double result = MyMath.divide(a, b);
+        double expectedResult = 1.6885245901639347;
+        Assertions.assertEquals(expectedResult,result, "Wrong result of method 'divide' ");
     }
 }
