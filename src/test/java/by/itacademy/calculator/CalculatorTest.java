e03a4bd69e34bb8ef728f578e31169b7aa3c2025
potapov.alexey <potapov.alex@hotmail.com>
2022-06-17 22:57:51 +0300

    expression recoding

--- src/test/java/by/itacademy/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/calculator/CalculatorTest.java
@@ -1,16 +1,26 @@
 package by.itacademy.calculator;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 import org.junit.Test;
 
-public class CalculatorTest
-{
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class CalculatorTest {
+
+    static double number = 4.0;
 
     @Test
-    public void shouldScanNumber()
-    {
+    public void scanNumberTesting() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Give a number between 1 and 10");
+        int input = keyboard.nextInt();
 
-        assertTrue( true );
+        double scanNumber = Calculator.setNumber("My number");
+        assertEquals(number, 1, "Scanned not a number");
     }
 }
